807c2918ccdcb6aca560ccd895cdf3a2db6ea9bc
Abel Diaz <abeldiaz2307@gmail.com>
2023-10-06 23:41:34 -0600

    Realizacion de backend de analizador sintatico de C++

--- lixsin/src/main/java/com/easdrox/lixsin/dao/LixsinDaoImpl.java
+++ lixsin/src/main/java/com/easdrox/lixsin/dao/LixsinDaoImpl.java
@@ -67,4 +67,97 @@ public class LixsinDaoImpl implements LixsinDao {
     private boolean isNumber(String token) {
         return token.matches("-?\\d+(\\.\\d+)?");
     }
+
+    @Override
+    public boolean analizadorSintactico(List<Token> tokens, Model model, String inputText) {
+        System.out.println("texto: "+inputText);
+        System.out.println("token: " + tokens.size());
+        boolean includeFound = false;
+        boolean namespaceFound = false;
+        boolean mainFound = false;
+        
+        for (int i = 0; i < tokens.size(); i++) {
+            if (!includeFound && isIncludeIOStream(tokens, i)) {
+                includeFound = true;
+                System.out.println("Encabezado #include <iostream> encontrado");
+            }
+    
+            if (!namespaceFound && isUsingNamespaceStd(tokens, i)) {
+                namespaceFound = true;
+                System.out.println("Declaración 'using namespace std;' encontrada");
+            }
+    
+            if (!mainFound && isMainFunction(tokens, i)) {
+                mainFound = true;
+                System.out.println("Función 'int main()' encontrada");
+            }
+        }
+        if(includeFound == true && namespaceFound == true && mainFound == true){
+            return true;
+        }else{
+            return false;
+        }
+    
+        // Puedes agregar más lógica después de la búsqueda de estas estructuras...   
+    }
+
+    private boolean isIncludeIOStream(List<Token> tokens, int index) {
+        return index + 1 < tokens.size() &&
+                tokens.get(index).getValue().equals("#include") &&
+                tokens.get(index + 1).getValue().equals("<iostream>");
+    }
+    
+    private boolean isUsingNamespaceStd(List<Token> tokens, int index) {
+        return index + 2 < tokens.size() &&
+                tokens.get(index).getValue().equals("using") &&
+                tokens.get(index + 1).getValue().equals("namespace") &&
+                tokens.get(index + 2).getValue().equals("std") &&
+                tokens.get(index + 3).getValue().equals(";");
+    }
+    
+    private boolean isMainFunction(List<Token> tokens, int index) {
+        int openingBraceIndex = findOpeningBrace(tokens, index);
+        if (openingBraceIndex != -1 && openingBraceIndex + 2 < tokens.size() &&
+            tokens.get(index).getValue().equals("int") &&
+            tokens.get(index + 1).getValue().equals("main()")) {
+            int closingBraceIndex = findClosingBrace(tokens, openingBraceIndex);
+           
+            // Buscar la cadena "Hola Mundo" dentro de las llaves de la función
+               return true;
+                
+            
+        }
+        return false;
+    }
+    
+
+    private int findOpeningBrace(List<Token> tokens, int index) {
+        // Buscar la llave de apertura '{' después de 'int main()'
+        for (int i = index; i < tokens.size(); i++) {
+            if (tokens.get(i).getValue().equals("{")) {
+                return i;
+            }
+        }
+        return -1; // Llave de apertura no encontrada
+    }
+
+    private int findClosingBrace(List<Token> tokens, int openingBraceIndex) {
+        int braceCount = 1;
+    
+        for (int i = openingBraceIndex + 1; i < tokens.size(); i++) {
+            if (tokens.get(i).getValue().equals("{")) {
+                braceCount++;
+            } else if (tokens.get(i).getValue().equals("}")) {
+                braceCount--;
+    
+                if (braceCount == 0) {
+                    return i; // Encuentra la llave cerrada correspondiente
+                }
+            }
+        }
+    
+        return -1; // Llave cerrada no encontrada
+    }
+
 }
+    
